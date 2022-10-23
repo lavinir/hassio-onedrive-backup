78dde89723dd1f30b2d834a422f75a30c1bfaacb
Nir Lavi <snirl@c-137.local>
2022-10-23 09:34:32 +0300

    sync

--- onedrive-backup/Hass/BackupManager.cs
+++ onedrive-backup/Hass/BackupManager.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using hassio_onedrive_backup.Contracts;
+using static hassio_onedrive_backup.Contracts.HassBackupsResponse;
 
 namespace hassio_onedrive_backup.Hass
 {
     internal class BackupManager
     {
+        private const string BackupFolder = "/backup";
+        private AddonOptions _addonOptions;
+
+        public BackupManager(AddonOptions addonOptions)
+        {
+            _addonOptions = addonOptions;
+        }
+
+        public static string GetBackupFilePath(Backup backup)
+        {
+            return $"{BackupFolder}/{backup.Slug}.tar";
+        }
+
+        public 
     }
 }
