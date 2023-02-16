17d556ccf42b313d36f2ac15b7fc34fd5265f7e1
Nir Lavi <nirlavi@microsoft.com>
2023-02-16 16:38:56 +0200

    save

--- onedrive-backup/Startup.cs
+++ onedrive-backup/Startup.cs
@@ -61,30 +61,3 @@ namespace hassio_onedrive_backup
         }
     }
 }
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllersWithViews();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseStaticFiles();
-//app.UseRouting();
-
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller}/{action=Index}/{id?}");
-
-//app.MapFallbackToFile("index.html");
-
-//app.Run();
