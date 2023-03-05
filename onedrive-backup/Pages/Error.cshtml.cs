d7c14360056abf23cbdeceac6db4f8a1df9571c7
Nir Lavi <snirl@c-137.local>
2023-03-05 15:19:31 +0200

    shift to blazor

--- onedrive-backup/Pages/Error.cshtml.cs
+++ onedrive-backup/Pages/Error.cshtml.cs
@@ -1,25 +1,27 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
 
-namespace onedrive_backup.Pages;
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-public class ErrorModel : PageModel
+namespace BlazorApp1.Pages
 {
-    private readonly ILogger<ErrorModel> _logger;
-
-    public ErrorModel(ILogger<ErrorModel> logger)
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
     {
-        _logger = logger;
-    }
+        public string? RequestId { get; set; }
 
-    public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        private readonly ILogger<ErrorModel> _logger;
 
-    public void OnGet()
-    {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
     }
 }
--- onedrive-backup/Program.cs
+++ onedrive-backup/Program.cs
@@ -1,3 +1,4 @@
+using BlazorApp1.Data;
 using hassio_onedrive_backup.Contracts;
 using hassio_onedrive_backup.Graph;
 using hassio_onedrive_backup.Hass;
@@ -9,17 +10,73 @@ namespace hassio_onedrive_backup
 {
     public class Program
     {
+        private const string clientId = "b8a647cf-eccf-4c7f-a0a6-2cbec5d0b94d";
+        private const string addonDirectory = "/data";
+        private static readonly List<string> scopes = new() { "Files.ReadWrite.AppFolder" };
+
+        private static Orchestrator _orchestrator;
+        private string _pathBase = "";
+        private string _indexContent;
+
         static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+#if DEBUG
+            IHassioClient hassIoClient = new HassioClientMock();
+            var addonOptions = AddonOptionsReader.ReadOptions();
+            // Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+#else            
+            ConsoleLogger.LogInfo(AppDomain.CurrentDomain.BaseDirectory);
+            Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*", SearchOption.TopDirectoryOnly).ToList().ForEach(ConsoleLogger.LogInfo);
+            Directory.SetCurrentDirectory(addonDirectory);
+            var addonOptions = AddonOptionsReader.ReadOptions();
+            string supervisorToken = Environment.GetEnvironmentVariable("SUPERVISOR_TOKEN")!;
+            IHassioClient hassIoClient = new HassioClient(supervisorToken, TimeSpan.FromMinutes(addonOptions.HassAPITimeoutMinutes));
+#endif
+            LocalStorage.InitializeTempStorage();
+            IGraphHelper graphHelper = new GraphHelper(scopes, clientId, (info, cancel) =>
+            {
+                ConsoleLogger.LogInfo(info.Message);
+                return Task.FromResult(0);
+            });
+
+            //var addonInfo = await hassIoClient.GetAddonInfo("local_hassio_onedrive_backup");
+            //_pathBase = addonInfo.DataProperty.IngressUrl;
+            //ConsoleLogger.LogInfo($"Ingress Info. Entry: {addonInfo.DataProperty.IngressEntry}. URL: {addonInfo.DataProperty.IngressUrl}");
+            //_indexContent = _indexContent.Replace("**baseurl**", addonInfo.DataProperty.IngressUrl);
+            // File.WriteAllText("index.html", indexContent);
+            _orchestrator = new Orchestrator(hassIoClient, graphHelper, addonOptions);
+            _orchestrator.Start();
+
+            SetUpWebUI(args);
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static void SetUpWebUI(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.WebHost.UseStaticWebAssets();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
             {
-                webBuilder.UseStartup<Startup>();
-            });
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
     }
 }
 
