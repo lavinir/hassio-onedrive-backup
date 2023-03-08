31b63401b0a599691170cfcb08c74c031c3ec752
Nir Lavi <nirlavi@microsoft.com>
2023-03-08 22:04:55 +0200

    save

---
+++ onedrive-backup/Extensions/BackupExtensions.cs
@@ -0,0 +1,37 @@
+﻿using hassio_onedrive_backup.Contracts;
+using onedrive_backup.Models;
+using static hassio_onedrive_backup.Contracts.HassBackupsResponse;
+
+namespace onedrive_backup.Extensions
+{
+    public static class BackupExtensions
+    {
+        public static BackupModel ToBackupModel(this Backup backup)
+        {
+            return new BackupModel
+            {
+                Slug = backup.Slug,
+                Date = backup.Date,
+                Name = backup.Name,
+                Type = backup.Type,
+                Size = backup.Size,
+                IsProtected = backup.Protected,
+                IsOnline = false
+            };
+        }
+
+        public static BackupModel ToBackupModel(this OnedriveBackup onedriveBackup)
+        {
+            return new BackupModel
+            {
+                Slug = onedriveBackup.Slug,
+                Date = onedriveBackup.BackupDate,
+                FileName = onedriveBackup.FileName,
+                Type = onedriveBackup.Type,
+                Size = onedriveBackup.Size,
+                IsProtected = onedriveBackup.IsProtected,
+                IsOnline = true
+            };
+        } 
+    }
+}
---
+++ onedrive-backup/Models/BackupModel.cs
@@ -0,0 +1,24 @@
+﻿namespace onedrive_backup.Models
+{
+    public class BackupModel
+    {
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool IsOnline { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Type { get; set; }
+
+        public bool IsProtected { get; set; }
+
+        public float Size { get; set; }
+
+        public int MyProperty { get; set; }
+
+    }
+}
