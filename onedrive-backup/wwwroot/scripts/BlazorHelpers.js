eb9948698da4beaae6ccae986b7728c689b08186
Nir Lavi <snirl@c-137.local>
2023-03-12 00:54:40 +0200

    save

--- onedrive-backup/wwwroot/scripts/BlazorHelpers.js
+++ onedrive-backup/wwwroot/scripts/BlazorHelpers.js
@@ -13,3 +13,16 @@
 function copyToClipboard(txt) {
     navigator.clipboard.writeText(txt);
 }
+
+function showAlert(message, type) {
+    const alertPlaceholder = document.getElementById('alertPlaceholder');
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
