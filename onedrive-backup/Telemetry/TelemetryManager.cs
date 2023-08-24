042a5a192f93218a8982cce3ea7820df38a7adfc
Nir Lavi <snirl@c-137.local>
2023-08-24 23:54:38 +0300

    save

--- onedrive-backup/Telemetry/TelemetryManager.cs
+++ onedrive-backup/Telemetry/TelemetryManager.cs
@@ -2,5 +2,10 @@
 {
 	public class TelemetryManager
 	{
+		private const string appId = "560e1794-229c-4e58-8b22-a1675e4db7dc";
+		private const string appKey = "guJ8Q~WDYfjPXgwT6yzs8aZT4kqruVGDxxFCEaQr";
+
+
+
 	}
 }
